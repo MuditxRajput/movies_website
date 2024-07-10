187075228a93748fcee0c532ccf7120ae94dcfea
Mudit Rajput <muditrajputpersonal@gmail.com>
2024-07-10 12:58:32 +0530

    'day2'

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Download Free Bollywood, Hollywood Movies & Web Series - FlimFusion",
-  description: "Explore and download the latest Bollywood, Hollywood movies, and web series for free. Get high-quality downloads and enjoy your favorite content without cost.",
-  keywords: "free movies download, Bollywood movies download, Hollywood movies download, web series download, HD movies download",
+  description:
+    "Explore and download the latest Bollywood, Hollywood movies, and web series for free. Get high-quality downloads and enjoy your favorite content without cost.",
+  keywords:
+    "free movies download, Bollywood movies download, Hollywood movies download, web series download, HD movies download",
 };
 
 export default function RootLayout({ children }) {
@@ -22,6 +24,20 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         {/* Add any additional meta tags or scripts needed */}
+        <meta
+          name="google-site-verification"
+          content="Ty91mF8MukUZ2n_4v4YxxiztWxZeKbe4JkuCSV0doA0"
+        />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-659N63Y67S"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-659N63Y67S');
+        </script>
       </Head>
 
       <html lang="en">
